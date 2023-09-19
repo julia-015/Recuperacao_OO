d4d65d7877b618d24be0d44a5cd070e3872d2112
julia-015 <jumd.vecchia@gmail.com>
2023-09-19 08:14:58 -0300

    update

--- Algoritmo.py
+++ Algoritmo.py
@@ -22,24 +22,24 @@ def main():
                 case 1:
                     print("Você está prestes a criar um novo cliente, insira as informações solicitadas.")
                     cliente = criar_cliente()
-                    banco.novo_cliente(cliente)
+                    banco.criar_conta(cliente)
                     print("Conta criada com sucesso!")
 
                 case 2:
-                    print("Você está prestes a excluir um cliente, insira as informações solicitadas.")
-                    excluir(banco)
+                    print("Você está prestes a realizar um saque.")
+                    sacar(banco)
 
                 case 3:
-                    print("Você está prestes a alterar um cliente")
-                    alterar(banco)
+                    print("Você está prestes a realizar um depósito.")
+                    deposito(banco)
 
                 case 4:
-                    print("Realizar Transferência")
+                    print("Você está prestes a realizar uma transferência")
                     transferencia(banco)
 
                 case 5:
-                    print("Realizar depósito")
-                    deposito(banco)
+                    print("Consultar Saldo:")
+                    consultar_saldo(banco)
 
                 case 6:
                     break
@@ -64,3 +64,45 @@ def criar_cliente():
 
     cliente = Cliente(nome, saldo_inicial)
     return cliente
+
+def sacar(banco):
+    nome = input("Digite o Nome do cliente: ")
+    cliente = banco.buscar_cliente(nome)
+    if cliente:
+        valor = float(input("Digite o valor do saque: "))
+        if valor <= cliente.saldo:
+            cliente.saldo -= valor
+            print("Saque realizado com sucesso!")
+        else:
+            print("Saldo insuficiente")
+
+def deposito(banco):
+    nome = input("Digite o Nome do cliente: ")
+    cliente = banco.buscar_cliente(nome)
+    if cliente:
+        valor = float(input("Digite o valor do depósito: "))
+        cliente.saldo += valor
+        print("Depósito realizado com sucesso!")
+
+def transferencia(banco):
+    nome_cliente = input("Digite o nome do cliente: ")
+    nome_destino = input("Digite o nome do Destinatário: ")
+    valor_envio = float(input("Digite o valor a depositar: "))
+
+    cliente_encontrado = banco.buscar_cliente(nome_cliente)
+    if cliente_encontrado:
+        cliente_destino = banco.buscar_cliente(nome_destino)
+        if cliente_destino:
+            cliente_encontrado.saldo -= valor_envio
+            cliente_destino.saldo += valor_envio
+            print(f"Transferência de R${valor_envio:.2f} realizada de {cliente_encontrado.nome} para {cliente_destino.nome}")
+        else:
+            print("Destinatário não encontrado")
+    else:
+        print("Remetente não encontrado")
+
+def consultar_saldo(banco):
+    nome = input("Digite o Nome do cliente: ")
+    cliente = banco.buscar_cliente(nome)
+    if cliente:
+        print(f"Saldo de {cliente.nome}: R${cliente.saldo:.2f}")
--- Classes.py
+++ Classes.py
@@ -1,7 +1,9 @@
 class Cliente:
-    def __init__(self, nome, saldo_inicial):
+    def __init__(self, nome, saldo_inicial, saldo=0):
         self.nome = nome
         self.saldo_inicial = saldo_inicial
+        self.saldo = saldo
+        self.saldo = self.saldo_inicial
 
     def getCliente(self):
         return self.nome, self.saldo_inicial
@@ -12,25 +14,48 @@ class Cliente:
     def setSaldo_Inicial(self, x):
         self.saldo_inicial = x
 
+    def setSaldo(self, x):
+        self.saldo = x
+
 class Banco():
 
     def __init__(self):
         self._clientes = {}
 
     def criar_conta(self, cliente):
+        conta = 0000
         self._clientes[cliente.nome] = cliente
         print(f"{cliente.nome} foi adicionado")
+        ContaCriada = conta +  1
+        return ContaCriada
+    
+    def buscar_cliente(self, nome):
+        return self._clientes.get(nome, None)
 
 class Transferencia():
 
-    def sacar(self, conta, valor):
-        pass
+    def __init__(self, cliente, saldo):
+        self._cliente = cliente
+        self._saldo = saldo
+        self._enviado = 0
+        self._recebido = 0
 
-    def depositar(self, conta, valor):
-        pass
+    def sacar(self, valor):
+        if valor <= self._saldo:
+            self._saldo -= valor
+            self._enviado += valor
+        else:
+            print("Saldo Insuficiente")
 
-    def transferir(self, origem, destino, valor):
-        pass
+    def deposito(self, valor):
+        self._saldo += valor
+        self._recebido += valor
+
+    def detalhes(self):
+        print(f'Cliente: {self._cliente.nome}\nSaldo: {self._saldo}')
+        if self._enviado > 0:
+            print(f"Sua transferência foi enviada! - {self._enviado}")
+        if self._recebido > 0:
+            print(f"Sua transferência foi recebida! - {self._recebido}")
+        print()
     
-    def saldo(self, conta):
-        pass
