1a0af442bfc84dfe1cdd473908638c0947f8e30a
julia-015 <jumd.vecchia@gmail.com>
2023-09-19 07:28:57 -0300

    update

---
+++ Classes.py
@@ -0,0 +1,18 @@
+class Banco:
+    def __init__(self):
+        pass
+
+    def criar_conta(nome, saldo_inicial):
+        pass
+
+    def sacar(conta, valor):
+        pass
+
+    def depositar(conta, valor):
+        pass
+
+    def transferir(origem, destino, valor):
+        pass
+    
+    def saldo(conta):
+        pass
